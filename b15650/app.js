fa4528dfd03347e4dae658f526448745a375ec22
HJ Kim <pknojlh27@naver.com>
2023-06-20 14:19:55 +0900

    15650 N과M (2)

--- b15650/app.js
+++ b15650/app.js
@@ -1,30 +1,27 @@
-const [N, M] = require('fs').readFileSync(__dirname+'/input.txt').toString().trim().split(' ').map(Number);
-let visited = new Array(N+1).fill(0);
+const [N, M] = require("fs")
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+let visited = new Array(N + 1).fill(0);
 let result = [];
-let answer = '';
 
-function dfs(v){
-    if(result.length === M){
-        for(let i=1; i<=N; i++){
-                answer += result.join(' ');
-                answer += "\n";
-            
-            return;
-        }
-        
+const dfs = (L, idx) => {
+  if (L === M) {
+    console.log(result.join(" "));
+  } else {
+    for (let i = idx; i <= N; i++) {
+      if (visited[i] === 0) {
+        visited[i] = 1;
+        result.push(i);
+        dfs(L + 1, i);
+        result.pop();
+        visited[i] = 0;
+      }
     }
-    else{
-        if(visited[v]===0){
-            visited[v] = 1;
-            result.push(v);
-            dfs(v+1);
-            visited[v] = 0;
-            result.pop();
-            dfs(v+1);
-        }
-        
-    }
-}
+  }
+};
 
-dfs(1);
-console.log(answer);
+dfs(0, 1);
