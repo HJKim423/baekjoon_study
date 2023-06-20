af54a2af6f7baab4336d10efc75300da34a15ce5
HJ Kim <pknojlh27@naver.com>
2023-06-20 15:00:35 +0900

    15655 N과M (6)

---
+++ b15655/app.js
@@ -0,0 +1,32 @@
+const input = require("fs")
+  .readFileSync(__dirname + "/input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+const [N, M] = [...input.shift().split(" ")].map(Number);
+const arr = input
+  .shift()
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+let result = [];
+let visited = Array.from({ length: N }, () => 0);
+
+const dfs = (L, idx) => {
+  if (L === M) {
+    console.log(result.join(" "));
+  } else {
+    for (let i = idx; i < N; i++) {
+      if (visited[i] === 0) {
+        visited[i] = 1;
+        result.push(arr[i]);
+        dfs(L + 1, i);
+        result.pop();
+        visited[i] = 0;
+      }
+    }
+  }
+};
+
+dfs(0, 0);
