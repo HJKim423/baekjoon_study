6727ce08df9d7e0f74d7cc41984f85965db7e317
HJ Kim <pknojlh27@naver.com>
2023-08-04 15:42:50 +0900

    feat: 프로그래머스 멀리 뛰기

---
+++ programmers/멀리뛰기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  var answer = 0;
+  let dp = Array.from({ length: n + 1 }, () => 0);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+  }
+  answer = dp[n];
+  return answer;
+}
+
+//전형적인 dp문제
