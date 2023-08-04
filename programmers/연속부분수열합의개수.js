06542d06ac6c5786b186a0ef43e6ca1d02b70a9a
HJ Kim <pknojlh27@naver.com>
2023-08-04 20:29:30 +0900

    feat: 프로그래머스 연속 부분 수열 합의 개수

---
+++ programmers/연속부분수열합의개수.js
@@ -0,0 +1,14 @@
+function solution(elements) {
+  var answer = 0;
+  let extendElem = [...elements, ...elements];
+  let result = [];
+
+  for (let j = 0; j < extendElem.length; j++) {
+    for (let i = 1; i <= len; i++) {
+      result.push(extendElem.slice(j, j + i).reduce((acc, cur) => acc + cur));
+    }
+  }
+
+  answer = new Set([...result]); //중복제거
+  return answer.size;
+}
